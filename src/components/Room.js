ff41b3ad42428dbd6b8b0db6ce3eef08e05cef82
Aleksi <aleksi.pohjonen@edu.turkuamk.fi>
2020-12-07 14:32:57 +0200

    Text for total paint cost & paint amount for room

--- src/components/Room.js
+++ src/components/Room.js
@@ -10,6 +10,8 @@ const Room = ( { name, size, typeFactor, paintName, paintPrice, paintCoverage, p
         <div>
             <p>{name} with a size of {size}m2 demands {Math.round(totalLitersNeeded * 100) / 100} liters of {paintName} - paint</p>
             <p>Cost {Math.round((totalCost * 100) / 100)}€</p>
+            <p>Total cost: x </p>
+            <p>Total paint needed: x </p>
         </div>
     )
 }
